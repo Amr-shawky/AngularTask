b1d4149a678d46b54c702dabdb58fc066d6152fb
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-28 15:31:31 +0300

    feat: Implement course filtering functionality and update component structure

--- src/app/features/user/Pages/courses/courses.component.ts
+++ src/app/features/user/Pages/courses/courses.component.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { CourseService } from '../../../../core/Services/course.service'; // Adjust path as per your project structure
 import { CoursesInfo } from '../../../../core/Models/api.interfaces'; // Adjust path
 import { CourseCardComponent } from '../../../../Shared/Components/course-card/course-card.component';
+import { BehaviorSubject } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [CommonModule, CourseCardComponent],
+  imports: [CommonModule, CourseCardComponent , FormsModule],
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
   courses: CoursesInfo[] = [];
+  filteredCourses: BehaviorSubject<CoursesInfo[]> = new BehaviorSubject<CoursesInfo[]>([]);
   errorMessage: string | null = null;
-
+  filterText: string = 'All';
   constructor(private courseService: CourseService) {}
 
   ngOnInit() {
@@ -26,6 +29,8 @@ export class CoursesComponent implements OnInit {
       next: (res: any) => {
         if (res.success) {
           this.courses = res.data as CoursesInfo[]; // Assuming data contains the array of courses
+          this.filteredCourses.next(this.courses);
+          console.log('Courses loaded:', this.courses);
         } else {
           this.errorMessage = res.message || 'Failed to load courses.';
         }
@@ -35,5 +40,16 @@ export class CoursesComponent implements OnInit {
         console.error(err);
       }
     });
+  }
+  filterCourses() {
+    if (this.filterText === 'All') {
+      this.filteredCourses.next(this.courses);
+    } else {
+      this.filteredCourses.next(this.courses.filter(course => course.price == 0));
+    }
+  }
+  freecourses() {
+    this.filterText = this.filterText === 'All' ? 'Free' : 'All';
+    this.filterCourses();
   }
 }
