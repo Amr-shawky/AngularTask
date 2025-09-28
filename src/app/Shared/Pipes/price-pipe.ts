3d24db2b75fcc9ba6318ea73ebf890fecb0b3e4a
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-09-28 15:37:43 +0300

    fix: Update price formatting in course card to use static currency string

--- src/app/Shared/Pipes/price-pipe.ts
+++ src/app/Shared/Pipes/price-pipe.ts
@@ -7,7 +7,7 @@ export class PricePipe implements PipeTransform {
 
   transform(price: number, priceString?: string): string {
     if (price > 0) {
-      return priceString || `$${price}`;
+      return  `${priceString} ${price}`;
     }
     return 'Free';
   }
